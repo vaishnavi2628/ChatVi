07b7c0ea37d939f498ea9f5d616e7da7c45b7e8a
vaishnavi2628 <vaishnavimishra840@gmail.com>
2026-05-26 17:38:28 +0530

    userroutes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.js"
+import userRoutes from "./routes/user.route.js"
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 dotenv.config();
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
+app.use("/api/users",userRoutes);
 app.listen(Port, ()=>{
     console.log("server is runnning ");
     connectDB();

1d8f950b9801ff8c960b1e0f89cf69dda9116fa6
vaishnavi2628 <vaishnavimishra840@gmail.com>
2026-05-30 21:56:33 +0530

    signup ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router'
+import { Navigate, Route, Routes } from 'react-router'
 import HomePage from './pages/HomePage.jsx'
 import SignUpPage from './pages/SignUpPage.jsx'
 import LoginPage from './pages/LoginPage.jsx'
@@ -8,20 +8,37 @@ import ChatPage from './pages/ChatPage.jsx'
 import OnboardingPage from './pages/OnboardingPage.jsx'
 import Notification from './pages/NotificationsPage.jsx'
 import {Toaster} from "react-hot-toast";
-import toast from "react-hot-toast";
+
+import { useQuery } from '@tanstack/react-query'
+import {axiosInstance} from './lib/axios.js'
 
 const App = () => {
+   //tanstack query
+   const {data:authData, isLoading , error}=useQuery({
+    queryKey:["authUser"],
+    queryFn:async ()=>{
+      const res = await axiosInstance.get("/auth/me");
+      return res.data;
+    },
+    retry:false,
+    //auth check if once failed it will reject it
+
+   });
+             
+   const authUser= authData?.user
+
   return (
+  //  const {data}= useQuery()
     <div className="h-screen " data-theme="night">
 
       <Routes>
-        <Route path='/' element={<HomePage/>}></Route>
-        <Route path='/signup' element={<SignUpPage/>}></Route>
-        <Route path='/login' element={<LoginPage/>}></Route>
-        <Route path='/notification' element={<Notification/>}></Route>
-        <Route path='/call'  element={<CallPage/>}></Route>
-        <Route path='/chat'  element={<ChatPage/>}></Route>
-        <Route path='/onboarding'  element={<OnboardingPage/>}></Route>
+        <Route path='/' element={ authUser?< HomePage/> : <Navigate to="/login"/>}></Route>
+        <Route path='/signup' element={!authUser ? < SignUpPage/> : <Navigate to="/"/>}></Route>
+        <Route path='/login' element={!authUser ?    <LoginPage/> : <Navigate to="/"/>}></Route>
+        <Route path='/notification' element={ authUser ?   <Notification/> : <Navigate to="/login"/> }></Route>
+        <Route path='/call'  element={ authUser ?    <CallPage/> : <Navigate to="/login"/>     }></Route>
+        <Route path='/chat'  element={ authUser ?    <ChatPage/> : <Navigate to="/login"/>    }></Route>
+        <Route path='/onboarding'  element={ authUser ?       <OnboardingPage/> : <Navigate to="/login"/>   }></Route>
       </Routes>
       <Toaster/> 
     </div>
---
+++ frontend/src/lib/api.js
@@ -0,0 +1,5 @@
+import { axiosInstance } from "./axios";
+export const signup=async (signupData)=>{
+    const response = await axiosInstance.post("/auth/signup",signupData);
+    return response.data;
+}
